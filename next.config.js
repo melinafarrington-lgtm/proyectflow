7cddf8c9d0da2e2b598a730a8a0fcc4c7d8a55d8
melinafarrington-lgtm <melina.farrington@gmail.com>
2026-05-27 18:14:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.experiments = { ...config.experiments, asyncWebAssembly: true };
+    return config;
+  },
 }
 
 module.exports = nextConfig
