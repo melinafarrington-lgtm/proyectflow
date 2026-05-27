6efdbb8c9df39fe7b48825591bef4a365dd00a1b
melinafarrington-lgtm <melina.farrington@gmail.com>
2026-05-27 13:21:02 -0300

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,2 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+export default function Document() { return ( <Html lang="es"><Head /><body className="bg-gray-50 text-gray-900"><Main /><NextScript /></body></Html> ) }
